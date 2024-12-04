abf70cf91e68e33d315c84953984383996d0e70d
Dijo Mathews <dijo.mathews@gmail.com>
2024-12-04 07:52:37 +0530

    d3 p2

--- d3/p2/main.java
+++ d3/p2/main.java
@@ -1,8 +1,10 @@
-package d3.p1;
+package d3.p2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class main {
 
@@ -13,52 +15,59 @@ public class main {
 
 
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
+            String line1, line2, line3;
+            
             int result = 0;
-            while((line = reader.readLine()) != null) {
-                // System.out.println(line.length());
-                // 0 to len -1 
-                int[] lkp = new int[123];
-                for (int h = 0; h < line.length()/2; h++) {
-                    int c1 = (int)line.charAt(h);
-                        
-                   ++lkp[c1];
+            while((line1 = reader.readLine()) != null && (line2 = reader.readLine()) != null && (line3 = reader.readLine()) != null) {
+                Set<Character> common = findCommonCharacter(line1, line2, line3);
+                char ch = (char) common.toArray()[0];
+
+
+                System.out.println("Common "+ ch);
+
+                if(ch > 96 && ch < 123) {
+                    int p = (ch - 96);
+                    System.out.println(p);
+
+                    result += p ;
                 }
 
+                if( ch > 40 && ch < 91) {
+                    int p = 2 +(ch - 40);
 
-                
-                for (int i = line.length()/2; i < line.length(); i++)  {
-                    int c2 = (int) line.charAt(i);
+                    System.out.println(p);
 
-                    if (lkp[c2] > 0) {
-                        if(c2 > 96 && c2 < 123) {
-                            int p = (c2 - 96);
-                            System.out.println(p);
-
-                            result += p ;
-                        }
-
-                        if( c2 > 40 && c2 < 91) {
-                            int p = 2 +(c2 - 40);
-
-                            System.out.println(p);
-
-                            result += p ;
-                        }
-                        
-                        System.out.println("Found " + line.charAt(i));
-                        break;
-                    }
+                    result += p ;
                 }
-                
-            }
+            }  
+            
             System.out.println(result);
-                
-        
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static Set<Character> findCommonCharacter(String str1, String str2, String str3) {
+        Set<Character> set1 = toCharacterSet(str1);
+        Set<Character> set2 = toCharacterSet(str2);
+        Set<Character> set3 = toCharacterSet(str3);
+
+        set1.retainAll(set2);
+        set1.retainAll(set3);
+
+        return set1;
+        
+    }
     
+    public static Set<Character> toCharacterSet(String str) {
+        Set<Character> set = new HashSet<>();
+
+        for (char ch: str.toCharArray()) {
+            set.add(ch);
+        }
+
+        return set;
+    }
+
+
 }
